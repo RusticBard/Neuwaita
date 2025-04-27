648e920be980ffc48e5fedc8a43187f2f87b96e0
RusticBard <sanket.p.kumbhar.2805@gmail.com>
2025-04-27 06:30:35 +0530

    changed the palette to encorporate the gnome accent color names

--- change-color.sh
+++ change-color.sh
@@ -4,14 +4,14 @@ colorName=$1
 colorNameDark="${colorName}-dark"
 
 if [ ${colorName} = 'reset' ]; then
-	cp ./scalable/places/folder-grey.svg ./scalable/places/folder.svg
+	cp ~/.local/share/icons/Neuwaita/scalable/places/folder-grey.svg ~/.local/share/icons/Neuwaita/scalable/places/folder.svg
 else
-    if grep -wq "${colorName}" ./Palette.txt ; then 
-        color=$(awk -v vawk="${1}" '$1==vawk{print $3}' Palette.txt)
-        colorDark=$(awk -v vawk="${1}-dark" '$1==vawk{print $3}' Palette.txt)
+    if grep -wq "${colorName}" ~/.local/share/icons/Neuwaita/Palette.txt ; then 
+        color=$(awk -v vawk="${1}" '$1==vawk{print $3}' ~/.local/share/icons/Neuwaita/Palette.txt)
+        colorDark=$(awk -v vawk="${1}-dark" '$1==vawk{print $3}' ~/.local/share/icons/Neuwaita/Palette.txt)
 
-        sed -i "64s/#[a-f0-9]\{6\}/#${color::-2}/g" ./scalable/places/folder.svg
-        sed -i "59s/#[a-f0-9]\{6\}/#${colorDark::-2}/g" ./scalable/places/folder.svg
+        sed -i "64s/#[a-f0-9]\{6\}/#${color::-2}/g" ~/.local/share/icons/Neuwaita/scalable/places/folder.svg
+        sed -i "59s/#[a-f0-9]\{6\}/#${colorDark::-2}/g" ~/.local/share/icons/Neuwaita/scalable/places/folder.svg
     else 
         echo "Invalid argument ..."
     fi
