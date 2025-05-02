79bb48f0f99fb7b7d6ed1204ee49ca611a92063c
RusticBard <sanket.p.kumbhar.2805@gmail.com>
2025-05-03 04:53:15 +0530

    Minor tweaks

--- watch-accent.sh
+++ watch-accent.sh
@@ -2,6 +2,5 @@
 
 gsettings monitor org.gnome.desktop.interface accent-color | while read -r line; do
     color=$(echo "$line" | cut -d':' -f2 | tr -d " '")
-
     ~/.local/share/icons/Neuwaita/change-color.sh $color
 done
